03227f9e77c1c074f1d6647ed4f319e7f89f088d
Sev Furneaux <sevfurneaux@gmail.com>
2022-10-15 20:30:44 +0100

    Remove .vc-config.json deletion

--- adapter.mjs
+++ adapter.mjs
@@ -1,14 +1,4 @@
-import { unlink } from "fs/promises";
 import fs from "fs";
-
-(async function (path) {
-  try {
-    await unlink(path);
-    console.log(`successfully deleted ${path}`);
-  } catch (error) {
-    console.error("there was an error:", error.message);
-  }
-})(".vercel/output/functions/render.func/.vc-config.json");
 
 fs.writeFile(
   ".vercel/output/functions/render.prerender-config.json",
