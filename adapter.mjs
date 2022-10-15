790dba67265d7b794e1197666e10a23ec2c41ad2
Sev Furneaux <sevfurneaux@gmail.com>
2022-10-15 21:28:09 +0100

    Remove fallback and update prerender config

--- adapter.mjs
+++ adapter.mjs
@@ -3,24 +3,10 @@ import fs from "fs";
 fs.writeFile(
   ".vercel/output/functions/render.prerender-config.json",
   JSON.stringify({
-    expiration: 5,
-    group: 1,
-    bypassToken: "2ec9172003a647b296f324848dd3d407",
-    fallback: {
-      type: "FileFsRef",
-      mode: 33188,
-      fsPath: "render.prerender-fallback.html",
-    },
+    expiration: 60,
+    bypassToken: "VeryLongAndVerySecretBypassToken",
+    allowQuery: undefined, // "If undefined each unique query value is cached independently"
   }),
-  function (err) {
-    if (err) throw err;
-    console.log("File is created successfully.");
-  }
-);
-
-fs.writeFile(
-  ".vercel/output/functions/render.prerender-fallback.html",
-  "Hello",
   function (err) {
     if (err) throw err;
     console.log("File is created successfully.");
