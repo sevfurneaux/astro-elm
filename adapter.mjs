9c60a3eaf6e460c071aa6066157044a22fcd3134
Sev Furneaux <sevfurneaux@gmail.com>
2022-10-15 21:41:01 +0100

    Revert adapter

--- adapter.mjs
+++ adapter.mjs
@@ -1,14 +1,18 @@
-// import fs from "fs";
+import fs from "fs";
 
-// fs.writeFile(
-//   ".vercel/output/functions/render.prerender-config.json",
-//   JSON.stringify({
-//     expiration: 60,
-//     bypassToken: "VeryLongAndVerySecretBypassToken",
-//     allowQuery: undefined, // "If undefined each unique query value is cached independently"
-//   }),
-//   function (err) {
-//     if (err) throw err;
-//     console.log("File is created successfully.");
-//   }
-// );
+fs.writeFile(
+  ".vercel/output/functions/render.prerender-config.json",
+  JSON.stringify(
+    {
+      expiration: 60,
+      bypassToken: "VeryLongAndVerySecretBypassToken",
+      allowQuery: undefined, // "If undefined each unique query value is cached independently"
+    },
+    null,
+    2
+  ),
+  function (err) {
+    if (err) throw err;
+    console.log("File is created successfully.");
+  }
+);
